48971b7d50b471c9815518b81b3c2618ec3a895a
Viacheslav Borysov <s.borisovbar@gmail.com>
2023-04-01 20:58:10 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
+import { useState } from 'react';
+import { ContactForm } from './phonebookForm/PhonebookForn';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { ContactList } from './contactsList/ContactList';
+import { Filter } from './filter/Filter';
+import  initialContacts  from './InitialContacts/initialContacts.json';
+import { useEffect } from 'react';
+
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem('contacts')
+  if (savedContacts !== null) {
+    const parsedContacts = JSON.parse(savedContacts);
+    return parsedContacts;
+  }
+  return initialContacts;
+};
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  const [contacts, setContacts] = useState(getInitialContacts);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  
+  const handleFilterInput = evt => { 
+    setFilter(evt.target.value)
+  }
+
+  const getFindContact = () => {
+    const normalizedFilter = filter.toLowerCase();
+   
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter)) 
+    
+  }
+
+  
+   const addContact = newContact => {
+    const normalizeName = newContact.name.toLowerCase();
+    contacts.find(
+      (contact) => contact.name.toLowerCase() === normalizeName
+    )
+      ? alert(`${normalizeName} is already on contacts`)
+      : setContacts((prewContacts) => [newContact, ...prewContacts]);
+  };
+
+  const deleteContact = contactId => { 
+    setContacts(pervState => { 
+      return (
+        pervState.filter(contact => contact.id !== contactId)
+      )
+    })
+  }
+
+    return (
+    <Layout>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter onChange={handleFilterInput} filter={filter} />
+      <ContactList contacts={getFindContact()} onDelete={deleteContact} />
+      <GlobalStyle />
+    </Layout>
+  )
 };
